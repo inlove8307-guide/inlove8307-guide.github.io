598b3dacf77f32944d6b86736e2cf3892c3ddb1f
Kim Taehoon <inlove8307@gmail.com>
2020-08-11 16:34:41 +0900

    update browser script

--- js/script.js
+++ js/script.js
@@ -18,9 +18,9 @@ javascript: window.load = function(){
     var style = document.createElement('style');
 
     style.textContent = '\
-    .anchors {position:absolute;top:0;left:0;z-index:10;padding:5px;}\
-    .anchor {display:inline-flex;justify-content:center;align-items:center;margin-top:4px;margin-right:4px;width:20px;height:20px;border-radius:5px;background-color:rgba(255, 255, 255, 0.5);font-size:14px;line-height:1;color:rgb(0, 0, 0)!important;}\
-    .anchor:hover {background-color:rgb(255, 255, 255, 1);}'.replace(/ /g, '');
+    .anchors {position:absolute;top:50%;left:50%;z-index:10;transform:translate(-50%, -50%);}\
+    .anchor {display:inline-flex;justify-content:center;align-items:center;margin-bottom:4px;margin-right:4px;width:20px;height:20px;border-radius:5px;background-color:rgba(255, 255, 255, 0.5);font-size:14px;line-height:1;color:rgb(0, 0, 0)!important;}\
+    .anchor:hover {background-color:rgb(255, 255, 255, 1);}';
 
     document.querySelector('body').appendChild(style);
 
@@ -35,10 +35,10 @@ javascript: window.load = function(){
         anchors = document.createElement('span');
         anchors.setAttribute('class', 'anchors');
         parent.appendChild(anchors);
-        url = ['https://www.flickr.com', event.target.href.match(/\/photos\/[-a-zA-Z0-9@:%._\-\+~#=]{1,50}\/[0-9]{1,20}\//g)[0], 'sizes/'].join('');
+        url = ['https://www.flickr.com', event.target.href.match(/\/photos\/[\w-_@]+\/[\d]+\//g)[0], 'sizes/'].join('');
 
         requestData(url, 'text', function(text){
-          var array = text.match(/\/photos\/[-a-zA-Z0-9@:%._\-\+~#=]{1,50}\/[0-9]{1,11}\/sizes\/[a-z0-9]{1,2}\//g)
+          var array = text.match(/\/photos\/[\w-_@]+\/[\d]+\/sizes\/[\w]+\//g)
             , anchor;
 
           while(array.length){
@@ -60,7 +60,7 @@ javascript: window.load = function(){
     document.addEventListener('click', function(event){
       if (event.target.classList.contains('anchor')) {
         requestData(event.target.getAttribute('href'), 'text', function(text){
-          var array = text.match(/live.staticflickr.com\/[0-9]{1,10}\/[a-z0-9_]{1,100}.[a-z]{3}/g)
+          var array = text.match(/live.staticflickr.com\/[\d]+\/[\w-_@]+.[\D]{3}/g)
             , url = ['https://', array[array.length-1]].join('');
 
           requestData(url, 'blob', function(blob){
@@ -95,7 +95,7 @@ javascript: window.load = function(){
     document.addEventListener('click', function(event){
       if (event.target.classList.contains('preview')) {
         requestData(event.target.href, 'text', function(text){
-          var array = text.match(/https:\/\/w.wallhaven.cc\/full\/[a-z0-9]{0,10}\/wallhaven-[a-z0-9]{0,10}.[a-z]{3}/g);
+          var array = text.match(/https:\/\/w.wallhaven.cc\/full\/[\w]+\/wallhaven-[\w]+.[\D]{3}/g);
           var url = array[0];
 
           requestData(url, 'blob', function(blob){
